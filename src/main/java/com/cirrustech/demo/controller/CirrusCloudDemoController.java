8138e5b5dade2cc7b715e452b3810586fb7c5a70
Cirrus Up Cloud <contact@cirrusup.cloud>
2018-12-17 06:55:13 +0200

    Distributed locking with ElastiCache Redis

--- src/main/java/com/cirrustech/demo/controller/CirrusCloudDemoController.java
+++ src/main/java/com/cirrustech/demo/controller/CirrusCloudDemoController.java
@@ -1,11 +1,16 @@
 package com.cirrustech.demo.controller;
 
+import com.cirrustech.demo.service.ReportService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +22,9 @@ public class CirrusCloudDemoController {
     @Value("${default.text}")
     private String defaultText;
 
+    @Autowired
+    private ReportService reportService;
+
     private static final Logger LOG = LoggerFactory.getLogger(CirrusCloudDemoController.class);
 
     @GetMapping("/hello")
@@ -24,5 +32,22 @@ public class CirrusCloudDemoController {
 
         LOG.info("/hello request.");
         return "Hello, " + name + ". This is my first REST service! " + defaultText;
+    }
+
+    @PostMapping("/report")
+    public ResponseEntity report(@RequestParam(value = "client", required = true) String client) {
+
+        LOG.info("/report request.");
+
+        try {
+            reportService.generateReport(client);
+            return ResponseEntity.ok("Report generation started");
+        } catch (IllegalArgumentException e) {
+
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (IllegalStateException e) {
+
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(e.getMessage());
+        }
     }
 }
